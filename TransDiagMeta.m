3f4efb60b177dc376c572dd11d8371e5340e34b5
wayalan <39726864+wayalan@users.noreply.github.com>
2019-02-01 16:53:26 +0800

    Update TransDiagMeta.m

--- TransDiagMeta.m
+++ TransDiagMeta.m
@@ -8,13 +8,13 @@
 
 %% 1) 
 %% identify the those FCs with survived the Bonferonni correction by meta-analysis on many datasets
-load fMRIDataSet_GPreBp_FDok_20170709_Yeo.mat
+load fMRIDataSet.mat
 
 %% Multiple Comparison Correction
 mcc=2; % 1:Bonferroni, 2:FDR, 3:uncorrected
 SigP=0.05; % p-value threshold
 
-%% calcluate FC on each site 
+%% calcluate FC on each site (dataset), here we have three sets: DEP:HC/BIP:HC/SCZ:HC
 rawFC = CalculateFCOnEachDataSite(fMRIDataSet);
 % rawFC{1}.patient and rawFC{1}.normal have been vectorized
 % number of FC's calculated
@@ -22,7 +22,7 @@ numFC = size(rawFC{1}.normal,2);
 % number of ROI's considered 
 numROI = size(fMRIDataSet{1}.normal{1},2);
 for i = 1 : length(fMRIDataSet)
-    % sample Size at each site
+    % Total sample size at each site
     sub1 = sum(fMRIDataSet{i}.id_n);
     sub2 = sum(fMRIDataSet{i}.id_p);
     sampleSize(i) =  sub1 + sub2;
@@ -30,16 +30,16 @@ end
 
 %% 2)
 %% meta-analysis to identify those FC's survived the Bonferoni correction  (iniFC)
-iniMetaResult = [];
-iniMetaResult_pt = [];
+iniMetaResult = []; % Comparison between patient and HC
+iniMetaResult_pt = []; % Comparison among patient groups only
 for i = 1 : numFC
     % calculate beta and variance and p value at each site for each link
     % between control and patients
     [beta, p_beta, var_beta, ~, effSize] = CalculateBetaOnEachDataSite(fMRIDataSet,rawFC, i);
     % calculate beta and variance and p value at each site for each link
-    % between patients
+    % between patient groups
     [beta_pt, p_beta_pt, var_beta_pt, ~, effSize_pt] = CalBetaBTPatients(fMRIDataSet,rawFC, i);
-    % meta for each link
+    % meta combined effect size for each link
     outputMeta = MetaAnalysis(beta, var_beta, p_beta, sampleSize, 3); % we need to report the direction of the group difference for each link, so we use fixed effect model   
     iniMetaResult(i,:) = [beta, p_beta, outputMeta.EffectSizeMeta, outputMeta.pMeta];   
     iniMetaResult_pt(i,:)=[beta_pt, p_beta_pt, effSize_pt];
@@ -52,27 +52,26 @@ end
 Site=[];
 n=ones(1,7);
 
-if mcc==1 %% Bonferroni
+if mcc==1 %% Bonferroni Correction
     mcP=SigP/numFC;
     mcP(1:4)=mcP;
-elseif mcc==2 %% FDR
-	% FDR for the p array of meta-analysis
-	mcP(4)=FDR(iniMetaResult(:,8),SigP);
+elseif mcc==2 %% FDR Correction
+    % FDR for the p array of meta-analysis
+    mcP(4)=FDR(iniMetaResult(:,8),SigP);
     % FDR for the p values between each of the two groups
-	mcP(1:3)=FDR(iniMetaResult(:,4:6),SigP);
-        
+    mcP(1:3)=FDR(iniMetaResult(:,4:6),SigP);
 elseif mcc==3 %% Uncorrected-P
     mcP=SigP;
     mcP(1:4)=mcP;
 end
 
 %% 3)
-%% Indentify the FC that satisfied the criteria of transdiagnostic and illness-specific model
+%% Indentify the FC that satisfied the criteria of transdiagnostic and illness-specific model (mcP(i): p-value of group pair i)
 for i = 1: numFC
-
-	% Transdiagnostic Result: differences found in each of the PT vs HC, as well as having same direction (iniMetaResult(i,8): meta-p)
+    
+    % Transdiagnostic Result: differences found in each of the PT vs HC, as well as having same direction (iniMetaResult(i,8): meta-p)
     if iniMetaResult(i,4) < mcP(1) && iniMetaResult(i,5) < mcP(2) && iniMetaResult(i,6) < mcP(3) && iniMetaResult(i,8) < mcP(4) 
-        Site.CoD(n(1,1),1) = i;
+        Site.Trans(n(1,1),1) = i;
         n(1,1)=n(1,1)+1;
 
     % Illness-specific Result: differences found in the first set of the PT vs HC, no significant difference can be found in any other group set.
@@ -103,12 +102,16 @@ for i = 1: numFC
         Site.twothree(n(1,7),1)=i;
         n(1,7)=n(1,7)+1;
     end
-end
+end    
 
-% PT1 vs HC
-    Site.All_1=find(iniMetaResult(:,4)<mcP(1));
-% PT2 vs HC
-    Site.All_2=find(iniMetaResult(:,5)<mcP(2));
-% PT3 vs HC
-    Site.All_3=find(iniMetaResult(:,6)<mcP(3));
-      
+% Re-identify the transdiagnostic and illness-specific dysconnectivity 
+CheckBtPt=[]; %iniMetaResult_pt(:,4): DEP vs. BIP; iniMetaResult_pt(:,5): DEP vs. SCZ; iniMetaResult_pt(:,4): BIP vs. SCZ
+CheckBtPt.CoD(:,1)=Site.CoD(sum(iniMetaResult_pt(Site.Trans,4:6)<0.05,2)==0); %no difference between three patient groups
+CheckBtPt.DEP(:,1)=Site.one(sum(iniMetaResult_pt(Site.one,4:5)<0.05,2)==2); %significant difference between: DEP/BIP and DEP/SCZ
+CheckBtPt.BIP(:,1)=Site.two(sum(iniMetaResult_pt(Site.two,4:2:6)<0.05,2)==2); %significant difference between: DEP/BIP and BIP/SCZ
+CheckBtPt.SCZ(:,1)=Site.three(sum(iniMetaResult_pt(Site.three,5:6)<0.05,2)==2); %significant difference between: DEP/SCZ and BIP/SCZ
+
+Site.CoD=CheckBtPt.CoD;
+Site.one=CheckBtPt.DEP;
+Site.two=CheckBtPt.BIP;
+Site.three=CheckBtPt.SCZ;      
